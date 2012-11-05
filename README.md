bace1148a6c3b9968c932f5366a075f26dabcf28
Ray Miller <rm7@sanger.ac.uk>
2012-11-05 10:20:05 +0000

    Support optional options as first argument to create-archive

--- README.md
+++ README.md
@@ -4,9 +4,27 @@ A simple Clojure wrapper anound java.util.zip for creating zip archives.
 
 ## Usage
 
-    (use 'ziputil.core)
+    (use '[ziputil.core :only [create-archive] :as ziputil])
 
-    (create-zip-file "foo.zip" "file1.txt" file1 "file2.txt" file2)
+    (create-archive "foo.zip" "file1.txt" file1 "file2.txt" file2)
+
+Passing a :prefix option will add that prefix to the filenames in the archive:
+
+    (create-archive {:prefix "some/dir/"} "foo.zip" "file1.txt" file1 "file2.txt" file2)
+
+is equivalent to:
+
+    (create-archive "foo.zip" "some/dir/file1.txt" file1 "some/dir/file2.txt" file2)
+
+The compression level may also be specified in the options:
+
+    (create-archive {:compression-level ziputil/best-compression} "foo.zip" "file1.txt" file1 "file2.txt" file2)
+
+## See Also
+
+fs - file system utilities for Clojure - provides similar
+functionality in fs.compression, but with a different API:
+<https://github.com/Raynes/fs>.
 
 ## License
 
--- src/ziputil/core.clj
+++ src/ziputil/core.clj
@@ -1,29 +1,39 @@
 (ns ziputil.core
   (:require [clojure.java.io :as io])
-  (:import [java.util.zip ZipEntry ZipOutputStream]))
+  (:import [java.util.zip ZipEntry ZipOutputStream Deflater]))
 
 (def ^:dynamic *buffer-size* 10240)
 
-(defn create-zip-file
+(def best-speed          Deflater/BEST_SPEED)
+(def best-compression    Deflater/BEST_COMPRESSION)
+(def default-compression Deflater/DEFAULT_COMPRESSION)
+
+(defn create-archive*
+  [output-file {:keys [compression-level prefix] :or {compression-level default-compression prefix ""}} entries]
+  (let [buffer (byte-array *buffer-size*)]
+    (with-open [zip-output-stream (ZipOutputStream. (io/output-stream output-file))]
+      (.setLevel zip-output-stream compression-level)
+      (doseq [[entry-name entry-data] (partition 2 entries)]
+        (.putNextEntry zip-output-stream (ZipEntry. (str prefix (name entry-name))))
+        (with-open [input (io/input-stream entry-data)]
+          (loop []
+            (let [bytes-read (.read input buffer 0 *buffer-size*)]
+              (when (not= -1 bytes-read)
+                (.write zip-output-stream buffer 0 bytes-read)
+                (recur)))))
+        (.closeEntry zip-output-stream))))
+  output-file)
+
+(defn create-archive
   "Write a compressed zip archive of `entries` to `output-file`. `entries` should
    be a list of `name`/`data` pairs, where `name` is the name of the zipfile entry and
    `data` anything that can be opened with clojure.java.io/input-stream. For
    example:
-
-  (create-zip-file \"foo.zip\" \"file1.txt\" file1 \"file2.txt\" file2)"
-  [output-file & entries]
-  (when (odd? (count entries))
-    (throw (IllegalArgumentException. "create-zip-file requires an even-numbered list of name/data pairs")))
-  (let [buffer (byte-array *buffer-size*)]
-    (with-open [zip-output-stream (ZipOutputStream. (io/output-stream output-file))]
-      (.setLevel zip-output-stream 9)
-      (doseq [[entry-name entry-data] (partition 2 entries)]
-        (.putNextEntry zip-output-stream (ZipEntry. (name entry-name)))
-        (with-open [input (io/input-stream entry-data)]
-          (loop []
-            (let [c (.read input buffer 0 *buffer-size*)]
-              (when (not= -1 c)
-                (.write zip-output-stream buffer 0 c)
-                (recur)))))
-        (.closeEntry zip-output-stream))))
-  true)
+  (create-arhcive \"foo.zip\" \"file1.txt\" file1 \"file2.txt\" file2)"
+  [output-file & args]
+  (let [[opts entries] (if (map? (first args))
+                         [(first args) (rest args)]
+                         [{} args])]
+    (when (odd? (count entries))
+      (throw (IllegalArgumentException. "create-archive requires an even-numbered list of name/data pairs")))
+    (create-archive* output-file opts entries)))
